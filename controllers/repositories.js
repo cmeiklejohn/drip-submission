5cd0e70bad34d788180fda40c985d896da1becf9
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-27 01:02:12 -0400

    All builds for repos, all repos for user.

--- controllers/repositories.js
+++ controllers/repositories.js
@@ -2,6 +2,16 @@ var Repository = require('.././models/repository.js').Repository;
 
 module.exports.list = function(request, response) { 
   Repository.find(function(err, repositories) { 
+    if(err) throw err;
+    response.send(repositories);
+  });
+};
+
+module.exports.show = function(request, response) { 
+  var owner_name = request.params.owner_name;
+
+  Repository.find({ owner_name: owner_name }, function(err, repositories) { 
+    if(err) throw err;
     response.send(repositories);
   });
 };
