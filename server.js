530a1bedcce1212f15ed7a13c2487d157ef41fc2
Barnaby Claydon <barnaby@swipely.com>
2011-08-26 22:04:27 -0400

    resque whitespace fix

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.configure('production', function(){
 // Resque
 //
 resque.connect({ 
- host: 'carp.redistogo.com', 
- port: 9198, 
- password: '675f1ab0bd9310846989e6ef326a6237'
+  host: 'carp.redistogo.com',
+  port: 9198,
+  password: '675f1ab0bd9310846989e6ef326a6237'
 });
 
 // MongoHQ connection
