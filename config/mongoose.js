31d0b6983afa2c3c3fe216757e39e161b9635e6b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-27 21:07:31 -0400

    Export the connection.

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
     app      = require('../server'),
     url      = app.set('credentials').mongohq.url;
 
-mongoose.connect(url, function(err) { 
+module.exports = mongoose.connect(url, function(err) { 
   if (err) throw err;
   console.log('Connected to MongoHQ');
 });
