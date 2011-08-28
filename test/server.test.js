93a2033ad4dc99a2497ce94190fb8c17fafe0d3f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-27 21:22:29 -0400

    Fix formatting, include databases so we can disconnect.

--- test/server.test.js
+++ test/server.test.js
@@ -1,13 +1,17 @@
-var app = require('../server'),
-    assert = require('assert');
+var app       = require('../server'),
+    assert    = require('assert'), 
+    mongoose  = require('../config/mongoose'),
+    redis     = require('../config/redis'),
+    resque    = require('../config/resque');
 
 module.exports = { 
-  'Load the index': function() { 
+  'Load the index': function() {
     assert.response(app, 
-        { url: '/' }, 
-        { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
-        function(response) { 
-          assert.includes(response.body, 'drip: the expresso testing service');
-        });
+      { url: '/' }, 
+      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
+      function(response) { 
+        assert.includes(response.body, 'Drip is continuous integration for expresso.');
+      }
+    );
   }
 };
