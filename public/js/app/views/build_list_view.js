a4a585cde8b0155a46b8e6289b9a7e7431dd9514
Simon Nielsen <r.hackr@gmail.com>
2011-08-28 02:56:20 +0200

    more mocked builds

--- public/js/app/views/build_list_view.js
+++ public/js/app/views/build_list_view.js
@@ -8,10 +8,18 @@ var BuildListView = Backbone.View.extend({
 
   render: function () {
     var el = this.el = $(this.el),
-        frag = $(_.template($("#build_list_template").html(), {}))
+        frag = $( $("#build_list_template").html() ),
+        latestBuildNode = frag.find(".latest_build"),
         listNode = frag.find(".build_list");
 
-    _.each(this.collection.toArray(), function (build) {
+    var lastestBuildView = new LatestBuildView({
+      model: this.collection.at(0)
+    });
+
+    latestBuildNode.append(lastestBuildView.render().el);
+
+    _.each(this.collection.toArray(), function (build, i) {
+      if (i === 0) { return }
       listNode.append(new BuildListItemView({
         model: build
       }).render().el);
@@ -34,10 +42,24 @@ var BuildListItemView = Backbone.View.extend({
 
   render: function () {
     var el = this.el = $(this.el);
-
     el.html(this.model.get("_id"));
-
     return this;
   }
 
+});
+
+var LatestBuildView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'latest_build_results',
+
+  initialize: function () {
+    _.bindAll(this);
+  },
+
+  render: function () {
+    var el = this.el = $(this.el);
+    this.el.addClass("success");
+    this.el.html(this.model.get("_id"));
+    return this;
+  }
 });
