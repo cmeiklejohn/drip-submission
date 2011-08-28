64be0372554d1d504f36982d9a55512a2b938148
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-28 18:14:47 -0400

    On startup, mark in progress builds as failed.

--- worker.js
+++ worker.js
@@ -164,6 +164,35 @@ var worker = resque.worker('builder', Jobs);
 
 worker.on('job', function(worker, queue, job) {
   console.log("Attempting job; worker: " + worker + " queue: " + queue + " job: " + job);
-})
+});
+
+Repository.find({}, function (err, repositories) { 
+  if(err) throw err;
+
+  console.log("Looking for hung builds.");
+
+  repositories.forEach(function(repository) { 
+
+    console.log("Found repo: " + repository.url);
+
+    repository.builds.forEach(function(build) { 
+
+      console.log("Found build: " + build.id);
+
+      // Force all builds as finished when we start the worker if they
+      // were mid stream.
+      if(build.running) { 
+        build.completed = true;
+        build.running = false;
+        build.successful = false;
+        build.finishedAt = Date.now();
+        repository.save(function (err) { 
+          if (err) throw err; console.log("Marked in-progress build as finished."); 
+        });
+      }
+
+    });
+  });
+});
 
 worker.start();
