277f01e0193b7ac92f60dbd76d67d9357ed65e3c
Simon Nielsen <r.hackr@gmail.com>
2011-08-28 19:43:32 +0200

    socketio

--- public/js/app/models/repository_list.js
+++ public/js/app/models/repository_list.js
@@ -1,1 +1,15 @@
-var RepositoryList = Backbone.Collection.extend({url: '/repositories'});
+var RepositoryList = Backbone.Collection.extend({
+  url: '/repositories',
+
+  initialize: function () {
+    this.socket = io.connect('http://localhost');
+    this.socket.on('repository', function (data) {
+      console.log('socket.io received data:' + data);
+    });
+  },
+
+  addOrUpdateRepository: function () {
+  }
+
+
+});
