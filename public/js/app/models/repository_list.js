340fed8e7b6c88292948378a21dbd27c93b6e392
Simon Nielsen <r.hackr@gmail.com>
2011-08-28 20:33:56 +0200

    initial socket work ..

--- public/js/app/models/repository_list.js
+++ public/js/app/models/repository_list.js
@@ -2,9 +2,25 @@ var RepositoryList = Backbone.Collection.extend({
   url: '/repositories',
 
   initialize: function () {
-    this.socket = io.connect('http://localhost');
-    this.socket.on('repository', function (data) {
+    var list = this;
+    window.socket.on('repository', function (data) {
       console.log('socket.io received data:' + data);
+
+      console.log(repo._id);
+      console.log(repository);
+
+      var repo = data.repository,
+          repository = list.get(repo._id);
+
+      if (repository) {
+        console.log("found");
+        repository.set(repository);
+      }
+      else {
+        console.log("new");
+        list.add(new Repository(repo));
+      }
+
     });
   },
 
